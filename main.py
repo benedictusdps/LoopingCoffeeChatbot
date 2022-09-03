7772c4ef36c63c0a5aead8819f1701a42e05b58f
Benedictus See <78637682+benedictusdps@users.noreply.github.com>
2022-09-03 13:37:42 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from utils import print_message, get_size, order_mocha, order_latte
+
+def coffee_bot():
+  print('Welcome to the cafe!')
+  order_drink = 'y'
+  drinks = []
+
+  while order_drink == 'y':
+    size = get_size()  
+    drink_type = get_drink_type()
+
+    drink = '{} {}'.format(size, drink_type)
+    print('Alright, that\'s a {}!'.format(drink))
+    drinks.append(drink)
+
+    while True:
+      order_drink = input('Would you like to order another drink? (y/n) ')
+      
+      if order_drink in ['y', 'n']:
+        break
+  
+  print('Okay, so you will have:')
+  for drink in drinks:
+    print('-',  drink)
+  
+  name = input('Can I get your name please? \n> ')
+  print('Thanks, {}! Your order will be ready shortly.'.format(name))
+
+def get_drink_type():
+  res = input('What type of drink would you like? \n[a] Brewed Coffee \n[b] Mocha \n[c] Latte \n> ')
+
+  if res == 'a':
+    return 'brewed coffee'
+  elif res == 'b':
+    return order_mocha()
+  elif res == 'c':
+    return order_latte()
+  else:
+    print_message()
+    return get_drink_type()
+
+coffee_bot()
